ef1d465a4e8ca57a014d9d0263f008a828c3d505
Ilya Kolomoytsev <i.kolomoytsev@yandex.ru>
2024-12-09 21:02:06 +0300

    Добавлены константы путей

--- myproject/settings.py
+++ myproject/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMP_DIR = BASE_DIR / 'temp'
+DATA_STORAGE_DIR = BASE_DIR / 'data'
+LIBREOFFICE_EXE = "libreoffice"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
