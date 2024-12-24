f4da436e23e8ffdc909a571d522911dd3cc86acd
Ilya Kolomoytsev <i.kolomoytsev@yandex.ru>
2024-12-25 01:46:19 +0300

    Сделано 2 странички, главная и со списком степени обучения.

--- timetable/urls.py
+++ timetable/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),  # Главная страница
+    path('choose_degree', views.choose_degree, name='choose_degree'),  # Главная страница
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -5,3 +5,29 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def choose_degree(request):
+    degrees = [
+        {
+            'title': 'Бакалавриат (специалитет)',
+            'url': 'schedule_bachelor_form_find.html',
+            'image': 'bachelor_image.png',
+            'css_class': 'degree-card-bachelor',
+            'separator_class': 'degree-card-separator-line-bachelor',
+        },
+        {
+            'title': 'Магистратура',
+            'url': 'schedule_master_form_find.html',
+            'image': 'master_image.png',
+            'css_class': 'degree-card-master',
+            'separator_class': 'degree-card-separator-line-master',
+        },
+        {
+            'title': 'Аспирантура',
+            'url': 'schedule_postgraduate_form_find.html',
+            'image': 'phd_image.png',
+            'css_class': 'degree-card-postgraduate',
+            'separator_class': 'degree-card-separator-line-postgraduate',
+        },
+    ]
+    return render(request, 'choose_degree.html', {'degrees': degrees})
